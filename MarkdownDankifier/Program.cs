df23d4f3ae98b32d8ef27f8eba6a809c463f1a66
cainy-a <cainatkinson98@gmail.com>
2020-04-29 15:50:48 +0100

    Add empty C# project

---
+++ MarkdownDankifier/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MarkdownDankifier
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
