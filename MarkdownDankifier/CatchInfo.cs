c26b93e05e4aa635f207febe415733f6e4fe6a43
cainy-a <cainatkinson98@gmail.com>
2020-05-01 15:47:05 +0100

    Refactor; improve code quality again

---
+++ MarkdownDankifier/CatchInfo.cs
@@ -0,0 +1,16 @@
+﻿namespace MarkdownDankifier
+{
+	public class CatchInfo
+	{
+		public CatchInfo(char characterToCatch, string characterName, string markdownFunction)
+		{
+			CharacterToCatch = characterToCatch;
+			CharacterName = characterName;
+			MarkdownFunction = markdownFunction;
+		}
+
+		public char CharacterToCatch;
+		public string CharacterName;
+		public string MarkdownFunction;
+	}
+}
--- MarkdownDankifier/Program.cs
+++ MarkdownDankifier/Program.cs
@@ -191,20 +191,6 @@ namespace MarkdownDankifier
         }
     }
 
-    public class CatchInfo
-    {
-        public CatchInfo(char characterToCatch, string characterName, string markdownFunction)
-        {
-            CharacterToCatch = characterToCatch;
-            CharacterName = characterName;
-            MarkdownFunction = markdownFunction;
-        }
-
-        public char CharacterToCatch;
-        public string CharacterName;
-        public string MarkdownFunction;
-    }
-
     public enum Fixes
     {
         Hyphen,
