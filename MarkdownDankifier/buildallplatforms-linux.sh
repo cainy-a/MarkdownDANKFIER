9cefab0161da4802a35e21aa169e66ce1fbd08eb
cainy-a <cainatkinson98@gmail.com>
2020-05-01 14:18:08 +0100

    ADD BUILD SCRIPTS FOR WIN MAC AND LINUX WOOOOOO

---
+++ MarkdownDankifier/buildallplatforms-linux.sh
@@ -0,0 +1,23 @@
+echo "Building for Windows 64-bit"
+dotnet publish -r win-x64
+
+echo "Building for Windows 32-bit"
+dotnet publish -r win-x86
+
+echo "Building for MacOS 64-bit"
+dotnet publish -r osx-x64
+
+echo "Building for Linux 64-bit"
+dotnet publish -r linux-x64
+
+echo "Opening Windows 64-bit build output directory"
+xdg-open ./bin/Debug/netcoreapp2.2/win-x64/publish
+
+echo "Opening Windows32-bit build output directory"
+xdg-open ./bin/Debug/netcoreapp2.2/win-x86/publish
+
+echo "Opening Linux 64-bit build output directory"
+xdg-open ./bin/Debug/netcoreapp2.2/linux-x64/publish
+
+echo "Opening MacOS 64-bit build output directory"
+xdg-open ./bin/Debug/netcoreapp2.2/osx-x64/publish
